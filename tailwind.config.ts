5c9e998756a73b59400b3fa7cededf41cfe4773a
Arinze Jeffrey <61787049+ArinzeJeffrey-droid@users.noreply.github.com>
2024-06-26 00:11:08 +0100

    [#vbCknrUP] Fix custom font not working (#3)
    * [#vbCknrUP] Fix custom font not working
    
    I added a Custom font in this ticket:
    [https://trello.com/c/CGPl9RHF/2-build-layout-component](https://trello.com/c/CGPl9RHF/2-build-layout-component "smartCard-inline")
    Turns out the fonts wasn’t loaded currently. Need to fix.
    
    https://trello.com/c/vbCknrUP

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,9 +18,6 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
-        body: ["Nunito", "Arial", "sans-serif"],
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
