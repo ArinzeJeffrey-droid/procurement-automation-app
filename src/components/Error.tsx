49939cddf803d4c78cdc583e90bf9733a49b14ec
Arinze Jeffrey <61787049+ArinzeJeffrey-droid@users.noreply.github.com>
2024-06-26 15:07:44 +0100

    [#9VOIic7Z] Render a text when there is no order (#16)
    * [#9VOIic7Z] Render a text when there is no order
    
    https://trello.com/c/9VOIic7Z
    
    * feat: edge case for no orders.

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,9 +1,9 @@
 export default function Error() {
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="bg-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
+      <div className="bg-red-500 text-white font-bold rounded-lg shadow-lg p-10">
         <h1 className="text-2xl">Error</h1>
-        <p>Something went wrong</p>
+        <p>Something went wrong, try refreshing your browser</p>
       </div>
     </div>
   );
